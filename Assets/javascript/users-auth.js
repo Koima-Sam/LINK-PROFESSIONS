f74a8519ed537c44a23bbb9bdec9e9eb090e3287
Koima-Sam <sam17koima@gmail.com>
2022-05-19 09:26:47 +0300

    make changes

--- Assets/javascript/users-auth.js
+++ Assets/javascript/users-auth.js
@@ -14,6 +14,12 @@ let users = {
     }
 };
 
+//Get users registration details
+var fullName = document.Register.fullname.value;
+var registerEmail = document.Register.userEmail.value;
+var password = document.Register.userPassword.value;
+var password2 = document.Register.userPassword2.value;
+
 
 function valUsers(){
 
@@ -54,4 +60,42 @@ function valUsers(){
             return false;
         }
     }
+}
+
+//Validate users Registering
+//Declaring the create user function
+function createUser(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(document.Register.fullname.value == ""){
+        alert("Please indicate your full name!");
+        document.Register.fullname.focus();
+
+    }
+    else if(document.Register.userEmail.value == ""){
+        alert("Please input a valid email address!");
+        document.Register.registerEmail.focus();
+        return false;
+    }
+    else if(document.Register.userPassword.value =="" || document.Register.userPassword2.value ==""){
+        alert("Please indicate your password!");
+        document.Register.password.focus();
+        document.Register.password2.focus();
+        return false;
+
+    }
+    else if(document.Register.userPassword.value != document.Register.userPassword2.value){
+        alert("Passwords are not matching!");
+        document.Register.password.focus();
+        document.Register.password2.focus();
+        return false;
+
+    }
+    else{
+        // var obj ={}
+        // obj.email = document.Register.userEmail.value ;
+        // obj.password = document.Register.userPassword.value;
+        
+        // Object.assign(users.password,)
+    }
+
 }
